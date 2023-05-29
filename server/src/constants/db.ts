6469c5bdbe84b44889c6004ab3e1f4d30cef4564
djgu <djgu@tmindtech.com>
2023-05-29 15:23:05 +0800

    fix: info

--- server/src/constants/db.ts
+++ server/src/constants/db.ts
@@ -5,7 +5,7 @@ const __DEV__ = process.env.NODE_ENV === 'development'
 export const DATABASE_INFO: PoolConfig = {
   host: 'localhost',
   user: 'root',
-  password: __DEV__ ? '12345678' : 'Gu1920948999',
+  password: __DEV__ ? '12345678' : 'Gdj!137159',
   database: 'Blog',
   port: 3306,
 };
