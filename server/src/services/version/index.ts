4a2de79b60628ed9296f80c262b92757d59584cc
djgu <djgu@tmindtech.com>
2023-07-18 16:12:04 +0800

    feat: session

--- server/src/services/version/index.ts
+++ server/src/services/version/index.ts
@@ -4,27 +4,27 @@ import versionService from "./service"
 @RequestMapping('/version')
 export default class Version {
 
-  @Get('/list')
+  @Get('/list', true)
   list(...args) {
     return versionService.list(...args)
   }
 
-  @Post('/create')
+  @Post('/create', true)
   create(...args) {
     return versionService.create(...args)
   }
 
-  @Put('/edit/:id')
+  @Put('/edit/:id', true)
   edit(...args) {
     return versionService.edit(...args)
   }
 
-  @Delete('/delete/:id')
+  @Delete('/delete/:id', true)
   delete(...args) {
     return versionService.delete(...args)
   }
 
-  @Post('/used/:id')
+  @Post('/used/:id', true)
   used(...args) {
     return versionService.updateUsedVersion(...args)
   }
