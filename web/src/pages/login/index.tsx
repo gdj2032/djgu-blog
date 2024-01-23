829bd0c5c1b0c94e996233f39b4e29b74bec9d50
djgu <djgu@tmindtech.com>
2024-01-23 15:50:48 +0800

    fix: ui

--- web/src/pages/login/index.tsx
+++ web/src/pages/login/index.tsx
@@ -20,54 +20,52 @@ function Login() {
   };
 
   return (
-    <PageFrame className={sys.mode} hideTitleBar={!isElectron}>
-      <div className="layout-login">
-        <div className="login-wrap">
-          <div className="login-content">
-            {/* <div className="project-name">DEMO</div> */}
-            <div className="login-form">
-              <div className="login-title">登录</div>
-              <Form
-                name="basic"
-                labelCol={{ span: 0 }}
-                initialValues={{ remember: true }}
-                onFinish={onFinish}
+    <div className="layout-login">
+      <div className="login-wrap">
+        <div className="login-content">
+          {/* <div className="project-name">DEMO</div> */}
+          <div className="login-form">
+            <div className="login-title">登录</div>
+            <Form
+              name="basic"
+              labelCol={{ span: 0 }}
+              initialValues={{ remember: true }}
+              onFinish={onFinish}
+            >
+              <Form.Item
+                label=""
+                name="username"
+                rules={[{ required: true, message: '请输入用户名' }]}
+                initialValue={Setting.username}
               >
-                <Form.Item
-                  label=""
-                  name="username"
-                  rules={[{ required: true, message: '请输入用户名' }]}
-                  initialValue={Setting.username}
-                >
-                  <Input placeholder="请输入用户名" prefix={<UserOutlined />} />
-                </Form.Item>
+                <Input placeholder="请输入用户名" prefix={<UserOutlined />} />
+              </Form.Item>
 
-                <Form.Item
-                  label=""
-                  name="password"
-                  rules={[{ required: true, message: '请输入密码' }]}
-                  initialValue={Setting.password}
-                >
-                  <Input.Password placeholder="请输入密码" prefix={<LockOutlined />} />
-                </Form.Item>
+              <Form.Item
+                label=""
+                name="password"
+                rules={[{ required: true, message: '请输入密码' }]}
+                initialValue={Setting.password}
+              >
+                <Input.Password placeholder="请输入密码" prefix={<LockOutlined />} />
+              </Form.Item>
 
-                <Form.Item name="remember" valuePropName="checked">
-                  <Checkbox>记住密码</Checkbox>
-                </Form.Item>
+              <Form.Item name="remember" valuePropName="checked">
+                <Checkbox>记住密码</Checkbox>
+              </Form.Item>
 
-                <Form.Item className="login-btn">
-                  <Button type="primary" htmlType="submit">登录</Button>
-                </Form.Item>
-                <Form.Item className="login-btn">
-                  <Button type="primary" onClick={() => navigate(PathConfig.home)}>取消</Button>
-                </Form.Item>
-              </Form>
-            </div>
-            {/* {process.env.NODE_ENV !== 'development' ? <div className="version-number" style={{ fontSize: 10, textAlign: 'right' }}>{`版本号：${VERSION}`}</div> : null} */}
+              <Form.Item className="ant-btn-item login-btn">
+                <Button type="primary" htmlType="submit">登录</Button>
+              </Form.Item>
+              <Form.Item className="ant-btn-item cancel-btn">
+                <Button type="primary" onClick={() => navigate(PathConfig.home)}>取消</Button>
+              </Form.Item>
+            </Form>
           </div>
+          {/* {process.env.NODE_ENV !== 'development' ? <div className="version-number" style={{ fontSize: 10, textAlign: 'right' }}>{`版本号：${VERSION}`}</div> : null} */}
         </div>
       </div>
-    </PageFrame>
+    </div>
   );
 }
 
